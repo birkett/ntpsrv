a21380a5fd2d9325ea0b84790ed2e020c437be1a
Anthony Birkett <antbir@gmail.com>
2022-06-11 21:59:47 +0100

    Build: Remove hard coded config paths.

--- versions.sh
+++ versions.sh
@@ -4,3 +4,7 @@ export BUILDROOT_VERSION=2022.02.2
 export BUILDROOT_ARCHIVE_TYPE=xz
 export LINUX_GIT_HASH=6b945e6f05d5cb0a299dbbb9bdd285aff330a204
 export TARGET_DTB=bcm2709-rpi-2-b.dtb
+
+PROJECT_ROOT=$(pwd)
+
+export PROJECT_ROOT
