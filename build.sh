97116e2269ec679ae6d95a08bd3163ffea0c66bd
Anthony Birkett <antbir@gmail.com>
2022-06-11 14:15:48 +0100

    BootFS + Build: Add bootfs build step.

--- build.sh
+++ build.sh
@@ -28,3 +28,32 @@ cd buildroot-"$BUILDROOT_VERSION" || exit
 # make nconfig
 
 make -j8
+
+cd output/images || exit
+
+if [ -d "boot" ]
+then
+    rm -rf boot
+fi
+
+mkdir boot
+
+cd boot || exit
+
+sudo cp ../../../../../boot/* .
+sudo cp ../zImage kernel.img
+sudo cp ../../build/linux-"$LINUX_GIT_HASH"/arch/arm/boot/dts/"$TARGET_DTB" .
+sudo cp ../../build/rpi-firmware-*/boot/bootcode.bin .
+sudo cp ../../build/rpi-firmware-*/boot/fixup_cd.dat .
+sudo cp ../../build/rpi-firmware-*/boot/start_cd.elf .
+
+tar -cf ../bootfs.tar .
+
+dd if=/dev/zero of=../bootfs.vfat bs=16M count=1
+sudo mkfs.vfat ../bootfs.vfat
+
+../../host/bin/mcopy -i ../bootfs.vfat * ::
+
+
+cp ../rootfs.ext2 ../../../../
+cp ../bootfs.vfat ../../../../
